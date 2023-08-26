16b3489a38015c272ed921373ff1395088ea6e01
ToMle101 <tle01068@gmail.com>
2023-08-26 15:05:22 +1000

    init

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ewans\source\repos\procatstination\venv
+set VIRTUAL_ENV=C:\Users\tle01\OneDrive\Documents\CS porjects\Hackathon project 2023\procatstination\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
